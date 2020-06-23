083ce7a0268264da24b62814f63e56c94c401026
jjm00 <heaven2100@naver.com>
2020-06-23 23:48:54 +0900

    initial commit

--- app/src/main/java/com/edrain/calendar_lazy/MainActivity.java
+++ app/src/main/java/com/edrain/calendar_lazy/MainActivity.java
@@ -2,13 +2,47 @@ package com.edrain.calendar_lazy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    String time,kcal,menu;
+    private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
+    Cursor cursor;
+    MaterialCalendarView materialCalendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.calendar_page);
+
+        materialCalendarView=(MaterialCalendarView)findViewById(R.id.calendarView);
+
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2018, 01, 01))
+                .setMaximumDate(CalendarDay.from(2030, 12, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator(),
+                oneDayDecorator);
     }
 }
